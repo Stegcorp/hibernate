35299a3f04049ac99d61f4612f51927dee4790dd
stegcorp <stegcorp@gmail.com>
2022-06-28 15:55:32 +0300

    master

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+import models.Gendrer;
+import models.Passport;
+import models.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml")
+                .build();
+
+        Metadata metadata = new MetadataSources(serviceRegistry)
+                .addAnnotatedClass(null)
+                .addAnnotatedClass(null)
+                .getMetadataBuilder()
+                .build();
+
+        SessionFactory sessionFactory = metadata
+                .getSessionFactoryBuilder()
+                .build();
+
+        Session session = sessionFactory.openSession();
+
+        session.beginTransaction();
+
+
+        session.getTransaction().commit();
+
+        session.close();
+        sessionFactory.close();
+
+
+    }
+}
